3fbeff1bd9f6448e3c35fe513f9086d42ca51b75
Matheus <freitas.monte9@gmail.com>
2024-07-06 16:14:35 -0300

    Todos os campos estão corretos, mas só funciona para um item

--- src/extract/spiders/netshoes.py
+++ src/extract/spiders/netshoes.py
@@ -6,20 +6,24 @@ class NetshoesSpider(scrapy.Spider):
     allowed_domains = ["www.netshoes.com.br"]
     start_urls = ["https://www.netshoes.com.br/tenis-performance/masculino"]
 
-    def get_attributes(self, response):
-        # old_price = response.css('span.listInCents-value::text').get()
-        # new_price = response.css('span.saleInCents-value::text').get()
-        # rating_number = response.css('div.link__average::text').get()
-        # reviews_amount = response.css('p.link__infos--number-of-reviews::text').get()
-        pass
+    def get_attributes(self, response, brand, name):
+        yield dict (
+            old_price = response.css('span.listInCents-value::text').get(),
+            new_price = response.css('span.saleInCents-value::text').get(),
+            rating_number = response.css('div.link__average::text').get(),
+            reviews_amount = response.css('p.link__infos--number-of-reviews::text').get(),
+            brand = brand,
+            name = name
+        )
 
     def parse(self, response):
         products = response.css('div.product-list__items.double-columns')
 
         for product in products:
-            partial_link = product.css('a.card__link::attr(href)').get()
-            yield{
-                'brand': product.css('a.card__link::attr(data-brand)').get(),
-                'name': product.css('a.card__link::attr(data-name)').get()
-            }
+            url = f'https://www.netshoes.com.br{product.css('a.card__link::attr(href)').get()}'
+            request = scrapy.Request(url=url, callback=self.get_attributes)
+            request.cb_kwargs['brand'] = product.css('a.card__link::attr(data-brand)').get()
+            request.cb_kwargs['name'] =  product.css('a.card__link::attr(data-name)').get()
+
+            yield request
         
